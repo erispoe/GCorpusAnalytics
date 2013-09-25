0c0d3065a61dca114606dad6a511ff5d5790e459
Thomas Favre-Bulle <thomas.favre-bulle@epfl.ch>
2013-09-25 14:04:36 +0200

    Object oriented and better null result control
    - Queries are now objects. It prepares for a future future SQL data
    format implementation.
    - Queries are now processed by null result. The list of queries with
    null result is processed several times. It controls for the regular
    erroneous null results.

--- GCorpusAnalytics.py
+++ GCorpusAnalytics.py
@@ -7,104 +7,157 @@ import sys
 import random
 
 def main():
-    
     if sys.argv[1].lower() == 'help':
         printHelp()
     else:
+        corpus = str(sys.argv[1]).lower()
+        y1 = int(sys.argv[2])
+        y2 = int(sys.argv[3])
+        it = int(sys.argv[4])
+    
+        #Creates the list of expressions to query from the expression file
+        expressionsfile = open(sys.argv[5])
+        expressions = []
+        for line in expressionsfile:
+            expressions.append(str(line).rstrip('\n'))
+
+        outfilepath = ''
         try:
-            corpus = str(sys.argv[1]).lower()
-            y1 = int(sys.argv[2])
-            y2 = int(sys.argv[3])
-            it = int(sys.argv[4])
+            outfilepath = sys.argv[6]
+        except:
+            pass
+        
+        queries = []
+        
+        #Create the list of datespans and one query object per datespan per expression
+        for span in makeDatelist(y1, y2, it):
+            d1 = span[0]
+            d2 = span[1]
+            for e in expressions:
+                queries.append(Query(corpus, d1, d2, e))
+        
+        #Query all queries with a null result
+        #When created, all queries have a null result, therefore all queries are processed the first time
+        #Sometime a query gets an erroneous null result (0), processing three times the list of null queries reduces the risk of false null results
+        for i in range(3):
+            qwz = queriesWithZero(queries)
+            if len(qwz) > 0:
+                random.shuffle(qwz)
+                for q in qwz:
+                    time.sleep(random.uniform(1, 3))
+                    q.makeQuery()
+                    
+        if outfilepath != '':
+            exportToCsv(queries, len(expressions), outfilepath)
+                
+class Query:
     
-            expressionsfile = open(sys.argv[5])
-            expressions = []
-            for line in expressionsfile:
-                expressions.append(str(line).rstrip('\n'))
+    def __init__(self, corpus, d1, d2, expression):
+        self.corpus = corpus
+        self.d1 = d1
+        self.d2 = d2
+        self.expression = expression
+        self.url = makeURL(self.corpus, self.expression, self.d1, self.d2)
+        self.num = 0
     
-            outfile = ''
-            try:
-                outfile = sys.argv[6]
-            except:
-                pass
-    
-            if corpus == 'books':
-                results = GBooksQuery(y1, y2, it, expressions, outfile)
-            else:
-                print """At the moment, the google corpora supported are:
-- books"""
-        except Exception, e:
-            print "Oups, there seems to be a problem somewhere: '" , e , "'"
-            try:
-                if e.code == 503:
-                    print 'Google temporarily blocked your queries, try again in a little while.'
-            except:
-                pass
-            print "To get help on how to use the script, type 'python GCorpusAnalytics.py help'"
+    def printArgs(self):
+        print self.corpus, self.d1, self.d2, self.expression, self.num
+        
+    def makeQuery(self):
+        self.num = getResults(self.url)
+        print self.d1.year, self.d2.year, self.expression, self.num
+        
+    def getNum(self):
+        return self.num
 
-def GBooksQuery(y1, y2, it, expressions, outfile=''):
-    #Parameters:
-    #y1: start year
-    #y2: end year
-    #it: interval of queries, in years
-    #expressions: a list of string of expression to find in google books
-    #outfile: file where the resuts are to be exported
+def queriesWithZero(queries):
+    #Returns a list of queries with a resultStats of zero
+    qwz = []
+    for q in queries:
+        if q.getNum() == 0:
+            qwz.append(q)
+    return qwz
 
-    wr = 0
-    wrc = 0
+def exportToCsv(queries, nex, outfilepath):
+    #Export a list of queries into to csv files, one wih the resultats, the other with the url, for controlling purposes
+    #queries: the list of query objects to export to csv, sorted
+    #nex: the number of expressions
+    #outfilepath: the path of the csv file to export
     
-    if outfile != '':
-        controlfile = open(outfile.rstrip('\n') + '_urlcontrol.csv', 'wb')
-        outfile = open(outfile, 'wb')
-        wr = csv.writer(outfile, quoting=csv.QUOTE_ALL)
-        wrc = csv.writer(controlfile, quoting=csv.QUOTE_ALL)
+    controlfilepath = outfilepath.rstrip('.csv') + '_urlcontrol.csv'
+    controlfile = open(controlfilepath, 'w')
+    outfile = open(outfilepath, 'w')
     
-    results = []
-    datamodel = [] 
+    wr = csv.writer(outfile, quoting=csv.QUOTE_ALL)
+    wrc = csv.writer(controlfile, quoting=csv.QUOTE_ALL)
+    
+    #Let's assume the queries are sorted
+    #Which they should be because they are created sorted and all operations take place on another instance of the list
+    
+    datamodel = []
     datamodel.append('Timespan')
-    datamodel = datamodel + expressions
-    results.append(datamodel)
-    printRow(datamodel)
     
-    if wr != 0:
-        wr.writerow(datamodel)
-        wrc.writerow(datamodel)
+    for i in range(nex):
+        datamodel.append(queries[i].expression)
         
-    expressions = makeSafe(expressions)
+    wr.writerow(datamodel)
+    wrc.writerow(datamodel)
     
-    for span in makeDatelist(y1, y2, it):
-        d1 = span[0]
-        d2 = span[1]
-        
+    while len(queries) > 0:
+        d1 = queries[0].d1
+        d2 = queries[0].d2
+        resultrow = []
+        urlrow = []
         strspan = str(d1.year)
         if d1.year != d2.year:
             strspan = str(d1.year) + '-' + str(d2.year)
-        result = [strspan]
-        urls = [strspan]
-        for e in expressions:    
-            num, url = resultStatsBooks(e, d1, d2)
-            result.append(num)
-            urls.append(url)
-            time.sleep(random.uniform(1, 3))
-        results.append(result)
-        printRow(result)
-        
-        if wr != 0:
-            wr.writerow(result)
-            wrc.writerow(urls)
-            
-    return results
-
-def makeSafe(expressions):
-    #Takes a list of strings and returns alist of html safe strings
-    safeexpr = []
-    for e in expressions:
-        e = urllib.quote(e, safe="%/:=&?~#+!$,;'@()*[]")
-        safeexpr.append(e)
-    return safeexpr
-
-def makeDatelist(y1, y2, it):
+        resultrow.append(strspan)
+        urlrow.append(strspan)
+        for i in range(nex):
+            resultrow.append(queries[0].num)
+            urlrow.append(queries[0].url)
+            del queries[0]
+        wr.writerow(resultrow)
+        wrc.writerow(urlrow)
     
+
+def getResults(url):
+    #Query Google to return the numbers of items corresponding to the query url
+    
+    headers={'User-Agent':randomUserAgent(),}
+    
+    request=urllib.Request(url,None,headers)
+    soup = BeautifulSoup(urllib.urlopen(request).read())
+    
+    resultStats = 0
+    if noResults(soup):
+        pass
+    else:
+        try:
+            if soup.find("div", {"id": "resultStats"}).contents[0].split(' ')[0] == 'About':
+                resultStats = int(soup.find("div", {"id": "resultStats"}).contents[0].split(' ')[1].replace(',',''))
+            else:
+                resultStats = int(soup.find("div", {"id": "resultStats"}).contents[0].split(' ')[0].replace(',',''))
+        except:
+            pass
+    
+    #Sometimes the results page returns a high number of results even though it only displays a few of them
+    if elementCounter(soup) < resultStats and elementCounter(soup) < 10 :
+        resultStats = elementCounter(soup)
+        
+    return resultStats
+
+def randomUserAgent():
+    #Return a random user agent from the ones listed in the file Useragents.txt
+    useragents = open('Useragents.txt').readlines()
+    return str(random.choice(useragents)).rstrip('\n')
+
+def makeSafe(expression):
+    #Takes a string and returns html safe strings
+    return urllib.quote(expression, safe="%/:=&?~#+!$,;'@()*[]")
+    
+def makeDatelist(y1, y2, it):
+    #Returns a list of time spans between two dates
     ny = y1
     datelist = []
     while ny < y2:
@@ -115,11 +168,10 @@ def makeDatelist(y1, y2, it):
         datelist.append(dates)
     return datelist
     
-
-def resultStatsBooks(expression, date1, date2):
-    #Query Google Books to return the numbers of items corresponding to the expression between the dates date1 and date2
+def makeURL(corpus, expression, date1, date2):
+    #Return the query URL
     
-    headers={'User-Agent':randomUserAgent(),}
+    expression = makeSafe(expression)
     
     YYYY1 = str(date1.timetuple()[0])
     MM1 = str(date1.timetuple()[1])
@@ -129,42 +181,27 @@ def resultStatsBooks(expression, date1, date2):
     MM2 = str(date2.timetuple()[1])
     DD2 = str(date2.timetuple()[2])
     
-    url = 'http://www.google.com/search?hl=en&newwindow=1&q=' + expression + '&safe=off&tbm=bks&tbs=bkt:b%2Ccdr%3A1%2Ccd_min%3A' + MM1 +'%2F' + DD1 + '%2F' + YYYY1 + '%2Ccd_max%3A' + MM2 + '%2F' + DD2 + '%2F' + YYYY2
+    if corpus == 'books':
+        return 'http://www.google.com/search?hl=en&newwindow=1&q=' + expression + '&safe=off&tbm=bks&tbs=bkt:b%2Ccdr%3A1%2Ccd_min%3A' + MM1 +'%2F' + DD1 + '%2F' + YYYY1 + '%2Ccd_max%3A' + MM2 + '%2F' + DD2 + '%2F' + YYYY2
+
+def elementCounter(soup):
+    #Returns the number of items in the first page of results
+    return len(soup.find_all("div", {"class": "rc"}))
     
-    request=urllib.Request(url,None,headers)
-    soup = BeautifulSoup(urllib.urlopen(request).read())
-    
-    resultStats = 0
+def noResults(soup):
+    #Returns True is the page contains a no results alter
+    r = False
     try:
-        if soup.find("div", {"id": "resultStats"}).contents[0].split(' ')[0] == 'About':
-            resultStats = int(soup.find("div", {"id": "resultStats"}).contents[0].split(' ')[1].replace(',',''))
-        else:
-            resultStats = int(soup.find("div", {"id": "resultStats"}).contents[0].split(' ')[0].replace(',',''))
+        if str(soup.find("div", {"id": "topstuff"}).find_all("div", {"class": "med"})[0].find_all("img")[0]['src']) == '/images/yellow-alert2x.png':
+            r = True
     except:
         pass
-        
-    if len(soup.find("div", {"id": "topstuff"}).contents) > 0:
-        resultStats = 0
-    
-    return resultStats, url
-    
-def printRow(list):
-    #Takes a list and prints it on a line, elements separated by commas
-    st = ''
-    for i in list:
-        st = st + str(i) + ', '
-    st = st[:-2]
-    print st
+    return r
     
 def printHelp():
     #Prints the README.md file as help
     helptext = open('README.md').read()
     print helptext
-    
-def randomUserAgent():
-    #Return a random user agent from the ones listed in the file Useragents.txt
-    useragents = open('Useragents.txt').readlines()
-    return str(random.choice(useragents)).rstrip('\n')
-    
+
 if __name__ == '__main__':
     main()

66558f79f279cf909832e4721cccca059625b107
Thomas Favre-Bulle <thomas.favre-bulle@epfl.ch>
2014-01-27 12:28:03 +0100

    First shot at setup.py
    Creation of a basic setup.py file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='GCorpusAnalytics',
+      version='0.1.0',
+      description='Serialize queries on the number of items in Google corpora',
+      author='Thomas Favre-Bulle',
+      author_email='thomas.favre-bulle@epfl.ch',
+      url='https://github.com/Erispoe/GCorpusAnalytics',
+      packages=['GCorpusAnalytics'],
+      long_description=open('README.md').read(),
+      install_requires=[
+          "urllib2 >= 2.7",
+          "bs4 >= 4.3.1",
+          "csv >= 1.0",
+          "json >= 2.0.9",
+      ],
+     )
